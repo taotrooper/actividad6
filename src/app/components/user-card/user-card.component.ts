4cd9462722c65448f4d027b1a9a524970f275265
Christel González Canales <kirisame@gmail.com>
2023-09-02 01:05:35 +0200

    Mejoras al método de borrado
    Se ha creado un método en el servicio (swalDeleteUser) que usa la librería sweetalert2 para crear un modal de confirmación. Este método recibe un parámetro de tipo User y llama internamente al método delete si el usuario confirma la acción, y gestiona la respuesta del servidor. Si el usuario es borrado correctamente, redirige a /home.
    Editadas las funciones de borrado en user-card y user-detail para que solamente llamen al nuevo método, delegando el alert y la gestión al servicio.
    Añadido un mensaje de validación faltante y cambiado el título de la página web

--- src/app/components/user-card/user-card.component.ts
+++ src/app/components/user-card/user-card.component.ts
@@ -1,8 +1,6 @@
 import { Component, Input, inject } from '@angular/core';
-import { Router } from '@angular/router';
 import { User } from 'src/app/interfaces/user.interface';
 import { UsersService } from 'src/app/services/users.service';
-import Swal from'sweetalert2';
 
 @Component({
   selector: 'app-user-card',
@@ -13,45 +11,9 @@ export class UserCardComponent {
 
   @Input() miUser: User | any;
   usersService = inject(UsersService);
-  router = inject(Router);
 
-  swalDeleteUser() {
-    Swal.fire({
-      title: '¿Estás seguro?',
-      text: `¿Deseas borrar al usuario ${this.miUser.first_name}?`,
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Sí',
-      cancelButtonText: 'Cancelar'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        this.usersService.delete(this.miUser._id)
-          .then((response) => {
-            if(!response.error) {
-              Swal.fire(
-                'Borrado',
-                `El usuario ${this.miUser.first_name} ${this.miUser.last_name} ha sido eliminado`,
-                'success'
-              );
-              this.router.navigate(["/home"]);
-            } else {
-              Swal.fire(
-                'Error',
-                response.error,
-                'error'
-              )
-            }
-          }).catch((error) => {
-            Swal.fire(
-              'Error',
-              error,
-              'error'
-            )
-          })
-      }
-    })
+  deleteUser() {
+    this.usersService.swalDeleteUser(this.miUser);
   }
 
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -2,6 +2,8 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../interfaces/user.interface';
 import { lastValueFrom } from 'rxjs';
+import { Router } from '@angular/router';
+import Swal from'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,7 @@ import { lastValueFrom } from 'rxjs';
 export class UsersService {
 
   httpClient = inject(HttpClient);
+  router = inject(Router);
   private baseUrl: string = "https://peticiones.online/api/users/";
   private baseUrlForPag: string = "https://peticiones.online/api/users?page=";
 
@@ -22,16 +25,51 @@ export class UsersService {
     return lastValueFrom(this.httpClient.get<any>(`${this.baseUrl}${id}`));
   }
 
-  /*getTotalPages(): number {
-    return 1;
-  }*/
-
   update(formValue: User): Promise<any> {
     return lastValueFrom(this.httpClient.put<User>(`${this.baseUrl}${formValue._id}`, formValue))
   }
 
   delete(id: string): Promise<any> {
     return lastValueFrom(this.httpClient.delete<any>(`${this.baseUrl}${id}`));
+  }
+
+  swalDeleteUser(miUser: User) {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: `¿Deseas borrar al usuario ${miUser.first_name}?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.delete(String(miUser._id))
+          .then((response) => {
+            if(!response.error) {
+              Swal.fire(
+                'Borrado',
+                `El usuario ${miUser.first_name} ${miUser.last_name} ha sido eliminado`,
+                'success'
+              );
+              this.router.navigate(["/home"]);
+            } else {
+              Swal.fire(
+                'Error',
+                response.error,
+                'error'
+              )
+            }
+          }).catch((error) => {
+            Swal.fire(
+              'Error',
+              error,
+              'error'
+            )
+          })
+      }
+    })
   }
 
   insert(formValue: User): Promise<any> {
